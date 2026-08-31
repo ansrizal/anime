04234e6a01fdaa1809671057341fd4abc9bb601d
ansrizal <52710464+ansrizal@users.noreply.github.com>
2026-08-31 13:32:37 +0700

    Update DonghubProvider.kt

--- DonghubProvider/src/main/kotlin/com/donghub/DonghubProvider.kt
+++ DonghubProvider/src/main/kotlin/com/donghub/DonghubProvider.kt
@@ -144,8 +144,6 @@ class DonghubProvider : MainAPI() {
                 newEpisode(link) {
                     this.name = epTitle
                     this.posterUrl = fixUrlNull(poster)
-                    // Tambahkan skip OP
-                    this.skipOp = true
                 }
             }.reversed()
 

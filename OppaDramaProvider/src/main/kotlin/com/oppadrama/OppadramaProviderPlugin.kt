1f11945a52f98cd760076e331ffc2c71fb7fee69
ansrizal <52710464+ansrizal@users.noreply.github.com>
2026-08-31 11:32:36 +0700

    Add files via upload

--- OppaDramaProvider/src/main/kotlin/com/oppadrama/OppadramaProviderPlugin.kt
+++ OppaDramaProvider/src/main/kotlin/com/oppadrama/OppadramaProviderPlugin.kt
@@ -3,7 +3,8 @@ package com.oppadrama
 import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
 
 @CloudstreamPlugin
 class OppadramaProviderPlugin : Plugin() {

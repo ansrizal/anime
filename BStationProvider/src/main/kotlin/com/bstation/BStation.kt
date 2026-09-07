5b50fc7a59b8c0546d84cb34eb0a91011582c926
ansrizal <ansrizal242@gmail.com>
2026-09-07 07:07:10 +0700

    update

--- BStationProvider/src/main/kotlin/com/bstation/BStation.kt
+++ BStationProvider/src/main/kotlin/com/bstation/BStation.kt
@@ -19,7 +19,10 @@ class BStation : MainAPI() {
     override val mainPage = mainPageOf(
         "" to "Populer",
         "timeline" to "Anime Schedule",
-        "?bstar_from=bstar-web.homepage.recommend.all" to "Direkomendasikan untukmu"
+        "anime" to "Anime",
+        "short-drama" to "Dracin",
+        "trending" to "Trending",
+        "?bstar_from=bstar-web.homepage.recommend.all" to "Direkomendasikan untukmu",
     )
 
     override suspend fun getMainPage(

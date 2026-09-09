74e25b03d9a7e42afba1a4cf3639544f51940460
ansrizal <52710464+ansrizal@users.noreply.github.com>
2026-09-09 17:01:25 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if (requested.id.id == "com.lagradost.cloudstream3.gradle") {
-                useModule("com.github.recloudstream:gradle:-SNAPSHOT")
+                useModule("com.github.recloudstream.cloudstream:library:v4.0.0")
             }
         }
     }

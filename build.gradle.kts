86e7d78df946883e338bfb0d0d777ecbbbf20642
ansrizal <52710464+ansrizal@users.noreply.github.com>
2026-09-09 17:03:01 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
-        classpath("com.github.recloudstream.cloudstream:library:v4.0.0")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
     }
 }
 
